c2f23023f4f4a5522a753f7af50531d9f17c5258
Zander Ehlert <86738298+Tzeboys@users.noreply.github.com>
2026-04-12 20:31:48 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -2,21 +2,26 @@ import math
 
 
 def add(a, b):
-   return a + b
-def subtract(a, b):
-   return a - b
-def multiply(a, b):
-   return a * b
-def divide(a, b):
-   if a == 0:
-       raise ZeroDivisionError("Cannot divide by zero")
-   return b / a
-def logarithm(a, b):
-   if a <= 0 or b <= 0 or a == 1:
-       raise ValueError("Invalid arguments")
-   return math.log(b, a)
-def exponent(a, b):
-   return a ** b
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or b <= 0 or a == 1:
+        raise ValueError("Invalid arguments")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
 
